28d61bab2bc1334baddaa95a25c3776a80ee3b95
thanhluu <thanhlt90@gmail.com>
2016-05-01 08:49:11 +0700

    first commit

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funcFactLabel: UILabel!
+    
+    @IBOutlet weak var funFactButton: UIButton!
+    
+    let factModel = FactModel()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        funcFactLabel.text = factModel.getRandomFact()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showFunFact() {
+        let randomColor = ColorModel().getRandomColor()
+        //let newColor = UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0)
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
+        funcFactLabel.text = factModel.getRandomFact()
+    }
+    
 }
 
